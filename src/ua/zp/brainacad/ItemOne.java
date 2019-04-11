806272707adee044b2a0ddccccc8d9e407da4643
makerw111 <maker_w111@ukr.net>
2019-04-11 21:13:10 +0300

    add itemOne ItemThree

---
+++ src/ua/zp/brainacad/ItemOne.java
@@ -0,0 +1,8 @@
+package ua.zp.brainacad;
+
+public class ItemOne {
+
+    public static void main(String[] args) {
+        System.out.println("Hello lab2");
+    }
+}
---
+++ src/ua/zp/brainacad/ItemThree.java
@@ -0,0 +1,23 @@
+package ua.zp.brainacad;
+
+/**
+ * This is my first java experience
+ *
+ * @author Mamochkin
+ * @see System
+ *
+ */
+
+public class ItemThree {
+
+    /**
+     * jkjk
+     * {@link String}
+     * @param args qwerty
+     */
+    public static void main(String[] args) {
+        System.out.println("H\ne\nl\nl\no\n");
+        System.out.println("W\to\tr\tl\td\n");
+    }
+
+}
